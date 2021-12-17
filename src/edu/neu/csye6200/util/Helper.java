849b983cbea2e906e34a45074cb24064fd6b1d09
jamesshah <jdshah18072000@gmail.com>
2021-12-16 19:56:40 -0500

    Students, Teachers being fetched.

--- src/edu/neu/csye6200/util/Helper.java
+++ src/edu/neu/csye6200/util/Helper.java
@@ -4,7 +4,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Helper {
+import org.bson.Document;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import static com.mongodb.client.model.Filters.eq;
+
+public class Helper {	
 	
 	public static Date getDateFromString(String dateString) {
 		SimpleDateFormat dateFormat =new SimpleDateFormat("MM/dd/yyyy");
@@ -16,5 +22,63 @@ public class Helper {
 		}
 		return null;
 	}
+	
+	public static String getStudentGroupId(String age) {
+		int ageInt = Integer.parseInt(age);
+		FindIterable<Document> docs = null;
 		
+		MongoCollection<Document> collection = DBConn.getInstance().getCollection("Groups");
+		
+		if(ageInt >= 6 && ageInt <= 12) {
+			docs = collection.find(eq("classId", "06-12"));
+			for(Document doc : docs) {
+				String[] studentsList = (String[])doc.get("students");
+				if(studentsList.length <= 4)
+					return doc.get("id").toString();
+			}
+			return "";
+		} else if(ageInt >= 13 && ageInt <= 24) {
+			docs = collection.find(eq("classId", "13-24"));
+			for(Document doc : docs) {
+				String[] studentsList = (String[])doc.get("students");
+				if(studentsList.length <= 5)
+					return doc.get("id").toString();
+			}
+			return "";
+		} else if(ageInt >= 25 && ageInt <= 35) {
+			docs = collection.find(eq("classId", "25-35"));
+			for(Document doc : docs) {
+				String[] studentsList = (String[])doc.get("students");
+				if(studentsList.length >= 6)
+					return doc.get("id").toString();
+			}
+			return "";
+		} else if(ageInt >= 36 && ageInt <= 47) {
+			docs = collection.find(eq("classId", "36-47"));
+			for(Document doc : docs) {
+				String[] studentsList = (String[])doc.get("students");
+				if(studentsList.length >= 4)
+					return doc.get("id").toString();
+			}
+			return "";
+		} else if(ageInt >= 48 && ageInt <= 59) {
+			docs = collection.find(eq("classId", "48-59"));
+			for(Document doc : docs) {
+				String[] studentsList = (String[])doc.get("students");
+				if(studentsList.length >= 4)
+					return doc.get("id").toString();
+			}
+			return "";
+		} else if(ageInt >= 60) {
+			docs = collection.find(eq("classId", "60"));
+			for(Document doc : docs) {
+				String[] studentsList = (String[])doc.get("students");
+				if(studentsList.length >= 15)
+					return doc.get("id").toString();
+			}
+			return "";
+		} else {
+			return "Invalid age";
+		}
+	}		
 }
